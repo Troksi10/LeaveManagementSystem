0df2db3dc8484100830821eb9493f3a8075224df
lLija <ilija.trokovski@gmail.com>
2024-09-08 13:47:36 +0200

    Add project files.

---
+++ LeaveManSystem/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC",
+                DateOfBirth = new DateTime(1975,05,05)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+    }
+}
