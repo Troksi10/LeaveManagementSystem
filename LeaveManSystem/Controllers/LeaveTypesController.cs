d4593ad0aa4fc8cd445069e1e1a739a609942840
lLija <ilija.trokovski@gmail.com>
2024-09-18 20:11:16 +0200

    Completed Leave Management Module

--- LeaveManSystem/Controllers/LeaveTypesController.cs
+++ LeaveManSystem/Controllers/LeaveTypesController.cs
@@ -21,7 +21,8 @@ namespace LeaveManSystem.Controllers
         // GET: LeaveTypes
         public async Task<IActionResult> Index()
         {
-            return View(await _context.LeaveTypes.ToListAsync());
+            var data = await _context.LeaveTypes.ToListAsync();
+            return View(data);
         }
 
         // GET: LeaveTypes/Details/5
